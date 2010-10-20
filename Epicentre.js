a5dae5d83532de307a823ee368963f0b45be9dd7
Cody Brocious <cody.brocious@gmail.com>
2010-10-20 01:39:19 -0400

    Rasterbars \o/

--- Epicentre.js
+++ Epicentre.js
@@ -1,25 +1,45 @@
-var canvas, ctx, ctime = 0, time;
+canvas=ctx=ctime=tstart=tstep=0;
+cw=640; ch=480;
+M = Math;
+dead = false;
 
 function timeUpdate() {
 	ntime = (new Date).getTime();
 	if(ctime != 0)
-		time = ntime - ctime;
+		tstep = ntime - ctime;
+	tstart += tstep;
 	ctime = ntime;
 }
 
-function rasterBars() {
+function line(x1, y1, x2, y2, c) {
+	if(c!=undefined) ctx.strokeStyle = c;
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.stroke();
+}
+
+function rasterBar(y, o, s) {
+	for(i = 0; i < 25; ++i)
+		line(0, y+i, cw, y+i, 'hsl(' + (i+o%360)*s + ', 100%, 50%)');
 }
 
 function main() {
 	timeUpdate();
 	
-	rasterBars();
+	ctx.clearRect(0, 0, cw, ch);
+	rasterBar(tstart/10 % ch, 0, 10);
+	
+	if(!dead) setTimeout(main, 1000/60);
 }
 
 function ready() {
 	canvas = document.getElementById('epicentre');
 	ctx = canvas.getContext('2d');
 	
-	timeUpdate();
-	setInterval(main, 1000/60);
+	main();
+}
+
+function stop() {
+	dead = true;
 }
